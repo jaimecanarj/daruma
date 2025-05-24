ade2f660a8c768aaff3553d233af05f2aceecbe1
Jaime <jaimecanarj@gmail.com>
2025-05-24 20:00:45 +0200

    Eliminados archivos innecesarios

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,29 +1,14 @@
 import type { PageProps } from '@inertiajs/core';
-import type { LucideIcon } from 'lucide-vue-next';
 import type { Config } from 'ziggy-js';
 
 export interface Auth {
     user: User;
 }
 
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavItem {
-    title: string;
-    href: string;
-    icon?: LucideIcon;
-    isActive?: boolean;
-}
-
 export interface SharedData extends PageProps {
     name: string;
-    quote: { message: string; author: string };
     auth: Auth;
     ziggy: Config & { location: string };
-    sidebarOpen: boolean;
 }
 
 export interface User {
@@ -35,5 +20,3 @@ export interface User {
     created_at: string;
     updated_at: string;
 }
-
-export type BreadcrumbItemType = BreadcrumbItem;
