a9bb814d345dee4e7f6215b944599135d81c00c8
Jaime <jaimecanarj@gmail.com>
2025-05-31 12:04:19 +0200

    Eliminado manejo modo oscuro sustituido por vueuse

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,7 +7,6 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
@@ -44,6 +43,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-// This will set light / dark mode on page load...
-initializeTheme();
