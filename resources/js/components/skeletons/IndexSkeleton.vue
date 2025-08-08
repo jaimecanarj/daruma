b6fc0612b504131c9d40a0145981896be5fcbfee
Jaime <jaimecanarj@gmail.com>
2025-08-08 13:10:48 +0200

    Actualizado skeleton para representar cambios en diseño grid/cards

--- resources/js/components/skeletons/IndexSkeleton.vue
+++ resources/js/components/skeletons/IndexSkeleton.vue
@@ -8,11 +8,11 @@ const height = computed(() => (props.size === 'sm' ? 'h-28' : 'h-60'));
 
 <template>
     <!--Skeleton de cuadrícula-->
-    <div v-if="display === 'grid'" class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+    <div v-if="display === 'grid'" class="grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7">
         <USkeleton v-for="item in 20" :key="item" class="relative aspect-[1/1.4142] h-full w-full rounded-md shadow-md" />
     </div>
     <!--Skeleton de tarjetas-->
-    <div v-else class="grid grid-cols-1 gap-4 md:grid-cols-2">
+    <div v-else class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         <USkeleton v-for="item in 20" :key="item" :class="[height, 'w-full']" />
     </div>
 </template>
