c6ccdd2115a294a1857292a48705bdc172a9ea8b
Jaime <jaimecanarj@gmail.com>
2025-08-08 19:15:58 +0200

    Añadido botón de inicio a layout de autenticación

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import logo from '@/assets/logo.svg';
 import DarkMode from '@/components/navbar/DarkMode.vue';
+import { router } from '@inertiajs/vue3';
 import { es } from '@nuxt/ui/locale';
 </script>
 
@@ -9,7 +10,7 @@ import { es } from '@nuxt/ui/locale';
         <UContainer class="flex h-screen items-center justify-center">
             <UCard variant="subtle" class="w-sm">
                 <div class="flex justify-between">
-                    <div class="flex items-center gap-1">
+                    <div class="flex cursor-pointer items-center gap-1" @click="router.visit('/')">
                         <img :src="logo" class="h-11 w-11" alt="logo" />
                         <p class="text-2xl font-semibold">Daruma</p>
                     </div>
